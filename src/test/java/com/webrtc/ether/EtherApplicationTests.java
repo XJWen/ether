3f881d9a5e03f74c94d3b3c0f9f15ff69318aeca
xjWen <2899807359@qq.com>
2019-03-23 19:15:42 +0800

    2019/3/23 Initial commit

---
+++ src/test/java/com/webrtc/ether/EtherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webrtc.ether;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
