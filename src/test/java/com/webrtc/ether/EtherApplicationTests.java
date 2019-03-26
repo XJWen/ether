b6e5f34140203693f23be112670583b8c624bd78
xjWen <2899807359@qq.com>
2019-03-26 23:10:28 +0800

    2019.3.26 demo测试

--- src/test/java/com/webrtc/ether/EtherApplicationTests.java
+++ src/test/java/com/webrtc/ether/EtherApplicationTests.java
@@ -4,21 +4,35 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EtherApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     final Logger logger = LoggerFactory.getLogger(getClass());
     @Test
-    public void contextLoads() {
-        logger.trace("这是trance日志");
+    public void contextLoads() throws SQLException {
+       /* logger.trace("这是trance日志");
         logger.debug("这是debug日志");
         logger.info("这是info日志");
         logger.warn("这是warn日志");
-        logger.error("这是error日志");
+        logger.error("这是error日志");*/
+
+        System.out.println(dataSource.getClass());
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 }
